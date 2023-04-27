952aa62abd0032a3428536a5ced9b31d7da985c9
Xotiic5703 <112813151+Xotiic5703@users.noreply.github.com>
2023-04-27 19:44:40 +0200

    Update macheck.bat

--- macheck.bat
+++ macheck.bat
@@ -1,2 +1,2 @@
 @echo off
-wmic path Win32_NetworkAdapter where "PNPDeviceID like '%%PCI%%' AND NetConnectionStatus=2 AND AdapterTypeID='0'" get MacAddress > macadd.txt
+wmic path Win32_NetworkAdapter where "PNPDeviceID like '%%PCI%%' AND NetConnectionStatus=2 AND AdapterTypeID='0'" get MacAddress > C:\Users\%username%\AppData\Local\Temp\macadd.txt
